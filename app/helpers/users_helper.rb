52b610ac6589a70b5a71e96b637d91aec7bf5397
Dariusz Wojtowicz <darospeed1992@gmail.com>
2017-03-26 05:07:05 -0700

    Rejestracja i logowanie

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, size: 80)
+    gravatar_id = Digest::MD5::hexdigest(user.email ? user.email.downcase : 'darospeed1992@gmail.com')
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.login, class: "gravatar")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   root 'home_pages#home'
+  resources :users
 end
