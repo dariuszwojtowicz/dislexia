fe93dd680bf8d2501c0f54c6ad27a1797bb1a56e
Dariusz Wojtowicz <darospeed1992@gmail.com>
2017-03-26 09:25:42 -0700

    Authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326120859) do
+ActiveRecord::Schema.define(version: 20170326155931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170326120859) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Super",
+			 surname: "Admin",
+			 login: "sa",
+             email: "darospeed1992@gmail.com",
+             password:              "sony4401",
+             password_confirmation: "sony4401",
+             admin: true)
